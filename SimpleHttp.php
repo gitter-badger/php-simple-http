a0cb95435d4eeef02ea93b9cb6d8bd496733186f
bpteam <bpteam22@gmail.com>
2015-07-03 23:08:51 +0300

    add tests and bug fixing

--- SimpleHttp.php
+++ SimpleHttp.php
@@ -12,38 +12,44 @@ namespace bpteam\SimpleHttp;
 
 use bpteam\DryText\DryPath;
 
-class SimpleHttp {
+class SimpleHttp
+{
     protected $options = [];
     protected $shame = 'http';
     protected $url;
     protected $context;
     protected $useProxy;
 
-    public function setOption($name, $value){
-        $methodName = 'set'.ucfirst($name);
-        if(method_exists(__CLASS__, $methodName)){
+    public function setOption($name, $value)
+    {
+        $methodName = 'set' . ucfirst($name);
+        if (method_exists(__CLASS__, $methodName)) {
             $this->$methodName($value);
         } else {
             $this->options[$name] = $value;
         }
     }
 
-    public function unSetOption($name){
+    public function unSetOption($name)
+    {
         unset($this->options[$name]);
     }
 
     /**
      * @return string
      */
-    public function getMethod() {
+    public function getMethod()
+    {
         return $this->options['method'];
     }
 
     /**
      * @param string $method GET or POST
      */
-    public function setMethod($method) {
-        switch($method){
+    public function setMethod($method)
+    {
+        $this->unSetOption('header');
+        switch ($method) {
             case 'POST' :
                 $this->options['method'] = 'POST';
                 $this->onPost();
@@ -57,77 +63,108 @@ class SimpleHttp {
     /**
      * @return string
      */
-    public function getHeader() {
+    public function getHeader()
+    {
         return $this->options['header'];
     }
 
     /**
-     * @param string $header
+     * @param string|array $header
      */
-    public function setHeader($header) {
-        $this->options['header'] = is_array($header) ? implode("\n",$header) : $header;
+    public function setHeader($header)
+    {
+        $this->options['header'] = $this->makeHeader($header);
+    }
+
+    public function addHeader($header)
+    {
+        $this->options['header'] .= $this->makeHeader($header);
+    }
+
+    protected function makeHeader($header)
+    {
+        return is_array($header) ? implode("\n", $header) : ($header . "\n");
     }
 
     /**
      * @return string
      */
-    public function getContent() {
+    public function getContent()
+    {
         return $this->options['content'];
     }
 
     /**
      * @param string $content
      */
-    public function setContent($content) {
+    public function setContent($content)
+    {
         $this->options['content'] = is_array($content) ? http_build_query($content) : $content;
+    }
+
+    public function getPost()
+    {
+        return $this->getContent();
+    }
+
+    public function setPost($post)
+    {
+        $this->setContent($post);
     }
 
     /**
      * @return int
      */
-    public function getTimeout() {
+    public function getTimeout()
+    {
         return $this->options['timeout'];
     }
 
     /**
      * @param int $timeout
      */
-    public function setTimeout($timeout) {
+    public function setTimeout($timeout)
+    {
         $this->options['timeout'] = $timeout;
     }
 
     /**
      * @return mixed
      */
-    public function getUrl() {
+    public function getUrl()
+    {
         return $this->url;
     }
 
     /**
      * @param mixed $url
      */
-    public function setUrl($url) {
+    public function setUrl($url)
+    {
         $this->url = $url;
     }
 
     /**
      * @return string
      */
-    public function getShame() {
+    public function getShame()
+    {
         return $this->shame;
     }
 
     /**
      * @param string $shame
      */
-    public function setShame($shame) {
-        switch($shame){
+    public function setShame($shame)
+    {
+        switch ($shame) {
             case 'https' || 'ssl':
                 $this->shame = 'ssl';
                 $this->onHTTPS();
                 break;
             default:
                 $this->shame = 'http';
+                $this->offHTTPS();
         }
         $this->shame = $shame;
     }
@@ -135,7 +172,8 @@ class SimpleHttp {
     /**
      * @param mixed $useProxy
      */
-    public function setUseProxy($useProxy) {
+    public function setUseProxy($useProxy)
+    {
         $this->useProxy = $this->setProxy($useProxy);
     }
 
@@ -143,12 +181,13 @@ class SimpleHttp {
      * @param bool|string $proxy
      * @return bool
      */
-    protected function setProxy($proxy) {
+    protected function setProxy($proxy)
+    {
         switch ((bool)$proxy) {
             case true:
-                if (is_string($proxy)){
-                    if(DryPath::isIp($proxy)){
-                        $this->options['proxy'] = 'tcp://'.$proxy;
+                if (is_string($proxy)) {
+                    if (DryPath::isIp($proxy)) {
+                        $this->options['proxy'] = 'tcp://' . $proxy;
                         //$this->setOption('request_fulluri', true);
                     }
                 }
@@ -163,34 +202,42 @@ class SimpleHttp {
     /**
      * @return mixed
      */
-    public function getUseProxy() {
+    public function getUseProxy()
+    {
         return $this->useProxy;
     }
 
-    protected function onHTTPS(){
+    protected function onHTTPS()
+    {
         $this->setOption('verify_peer', false);
     }
 
-    protected function offHTTPS(){
+    protected function offHTTPS()
+    {
         $this->unSetOption('verify_peer');
     }
 
-    protected function onPost(){
+    protected function onPost()
+    {
+        $this->setHeader('Content-Type: application/x-www-form-urlencoded');
         $this->setOption('content', '');
     }
 
-    protected function offPost(){
+    protected function offPost()
+    {
         $this->unSetOption('content');
     }
 
-    protected function init(){
+    protected function init()
+    {
         $this->context = stream_context_create([$this->getShame() => $this->options]);
     }
 
 
-    public function load($url){
+    public function load($url)
+    {
         $this->init();
-        return file_get_contents($url, NULL, $this->context);
+        return file_get_contents($url, null, $this->context);
     }
 
 }
--- tests/SimpleHttpTest.php
+++ tests/SimpleHttpTest.php
@@ -10,7 +10,61 @@
 
 namespace bpteam\SimpleHttp;
 
+use \PHPUnit_Framework_TestCase;
+use \ReflectionClass;
 
-class SimpleHttpTest {
+class SimpleHttpTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @param        $name
+     * @param string $className
+     * @return \ReflectionMethod
+     */
+    protected static function getMethod($name, $className = 'bpteam\SimpleHttp\SimpleHttp')
+    {
+        $class = new ReflectionClass($className);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
 
+    /**
+     * @param        $name
+     * @param string $className
+     * @return \ReflectionProperty
+     */
+    protected static function getProperty($name, $className = 'bpteam\SimpleHttp\SimpleHttp')
+    {
+        $class = new ReflectionClass($className);
+        $property = $class->getProperty($name);
+        $property->setAccessible(true);
+        return $property;
+    }
+
+    public function testLoad()
+    {
+        $shttp = new SimpleHttp();
+        $url = 'http://ya.ru';
+        $text = $shttp->load($url);
+        $this->assertRegExp('%yandex%ims', $text);
+    }
+
+    public function testSendPost()
+    {
+        $shttp = new SimpleHttp();
+        $url = 'http://bpteam.net/post_test.php';
+        $post = ['url' => 'vk.com', 'test' => 'test_post'];
+        $shttp->setMethod('POST');
+        $shttp->setPost($post);
+        $text = $shttp->load($url);
+        $this->assertRegExp('%test_post%ims', $text);
+    }
+
+    public function testSetShame()
+    {
+        $shttp = new SimpleHttp();
+        $url = 'https://vk.com';
+        $shttp->setShame('https');
+        $text = $shttp->load($url);
+        $this->assertRegExp('%vk\.com%ims', $text);
+    }
 }
